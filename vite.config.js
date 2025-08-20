9e772b4192614324fccebbea240c92e36566a6d0
Sklassics <sklassicstechnologies@gmail.com>
2025-08-20 23:21:27 +0530

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    {
+      name: "log-errors",
+      buildEnd(err) {
+        if (err) {
+          console.error("ROLLUP BUILD ERROR:\n", err);
+        }
+      }
+    }
+  ]
 });
