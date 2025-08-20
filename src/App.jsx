7d36fabc458c9484f829b108130764af6d7e738d
Sklassics <sklassicstechnologies@gmail.com>
2025-08-20 13:59:40 +0530

    section/Navbar adjusted

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./sections/navbar";
+import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Projects from "./sections/Projects";
@@ -7,6 +7,8 @@ import Experiences from "./sections/Experiences";
 import Testimonial from "./sections/Testimonial";
 import Contact from "./sections/Contact";
 import Footer from './sections/Footer';
+
+
 
 const App = () => {
   return (
